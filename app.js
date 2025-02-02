1f3343d1e8827f83981f96b0792904030357caca
binetougaye <fabigaye3@gmail.com>
2025-02-02 15:23:24 +0000

    feat: les structures conditionnelles

---
+++ app.js
@@ -0,0 +1,77 @@
+// Les Opérateurs arithmétiques
+// Addition
+let a = 12
+let b = 13
+// console.log(a + b);
+// Soustraction
+// console.log(b - a);
+// Multiplication
+// console.log(a * b);
+// Division
+// console.log(b / a);
+// Modulo
+// console.log(b % a);
+// Puissance
+// console.log(b ** a);
+
+// Les Opérateurs de comparaison
+// x est de type number
+let x = 24
+// y est de type string
+let y = "24"
+// Supérieur >
+// console.log(x > y);
+
+// Supérieur ou égal >=
+// console.log(x >= y);
+// Inférieur <
+// console.log(x < y);
+// Inférieur ou égal <=
+// console.log(x <= y);
+
+// Egalité en valeur ==
+// console.log(x == y);
+
+// Egalité stricte ===
+// console.log(x === y);
+
+// Différent en valeur !=
+console.log(x != y);
+
+// Différent en valeur et type !==
+console.log(x !== y);
+
+
+
+// Opérateurs Logiques
+// &&
+let z = true
+let w = false
+// Si l'une des conditions n'est pas vérifiée toute la condition sera fausse
+console.log(z && w);
+// let age = 20
+let num = 30
+let nums = 32
+let ages = 42
+// console.log((age + num) === 50 && (nums + ages) <= 74);
+// || Si une des conditions est vérifiée la condition devient vraie
+// console.log((age + num) === 50 || (nums + ages) <= 74);
+
+// Les Structures Conditionnelles
+// If else else if
+let age = 18
+if (age == 18) {
+    alert('Vous êtes majeur')
+}
+else if (age == 0 || age <= 17) {
+    alert('Vous êtes un ado');
+
+} else {
+    alert('Vous êtes mineur');
+
+}
+
+
+
+
+
