0a17fedec92dcd97376400fca67b5ad5c0b0ddb7
binetougaye <fabigaye3@gmail.com>
2025-02-16 12:58:36 +0000

    feat:boucles en js

---
+++ boucles.js
@@ -0,0 +1,39 @@
+// Les Boucles 
+// Définition
+
+// La boucle For
+// for (let i = 0; i < 11; i++) {
+
+
+// }
+
+// La boucle While
+let i = 5
+// while (i < 10) {
+//     console.log(i);
+//     i++
+
+// }
+
+// Do while
+// do {
+//     console.log(i);
+//     i++
+
+// } while (i > 6);
+
+// For...of
+// const tab = ['banane', 'orange', 'fraise']
+// for (const fruits of tab) {
+//     console.log(fruits);
+
+// }
+
+
+// // Afficher en console les multiples de 10 jusqu'à 1000
+
+function greetings() {
+    alert("hello world!")
+
+}
+greetings()
